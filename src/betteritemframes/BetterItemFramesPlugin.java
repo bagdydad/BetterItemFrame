61a9e44f586cb27831c12a4ed8b83e8ab5496688
oTuin <60294079+bagdydad@users.noreply.github.com>
2022-11-12 13:51:53 +0300

    Source code update.

--- src/betteritemframes/BetterItemFramesPlugin.java
+++ src/betteritemframes/BetterItemFramesPlugin.java
@@ -1,10 +1,16 @@
 package betteritemframes;
 
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.block.Barrel;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.Chest;
+import org.bukkit.block.Lidded;
 import org.bukkit.block.ShulkerBox;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.ItemFrame;
@@ -12,12 +18,18 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BetterItemFramesPlugin extends JavaPlugin implements Listener {
+
+	HashMap<UUID,Location> enderChest = new HashMap<UUID,Location>();
 
 	@Override
 	public void onEnable() {
@@ -30,27 +42,59 @@ public class BetterItemFramesPlugin extends JavaPlugin implements Listener {
 	public void onDisable() {
 	}
 
-	@EventHandler(priority = EventPriority.LOWEST)
-	public void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
+	@EventHandler(ignoreCancelled = true)
+	private void onPlayerInteractEntity(PlayerInteractEntityEvent event) {
 
-		if (event.isCancelled()) return;
-		if (!event.getRightClicked().getType().toString().contains("ITEM_FRAME")) return;
-
-		if (event.getPlayer().isSneaking()) return;
+		if (event.isCancelled() || event.getRightClicked() == null || !event.getRightClicked().getType().toString().contains("ITEM_FRAME") || !event.getHand().equals(EquipmentSlot.HAND) || event.getPlayer().isSneaking()) return;
 
 		event.setCancelled(true);
 
-		if (!event.getHand().equals(EquipmentSlot.HAND)) return;
-
 		Block block = event.getRightClicked().getLocation().getBlock().getRelative(event.getRightClicked().getFacing().getOppositeFace());
 		Player player = event.getPlayer();
+
+		openInventory(block, player);
+
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	private void onPlayerInteract(PlayerInteractEvent event) {
+
+		if (event.getClickedBlock() == null || !event.getClickedBlock().getType().toString().contains("SIGN") || !event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getPlayer().isSneaking()) return;
+
+		event.setCancelled(true);
+
+		Block block = event.getClickedBlock().getLocation().getBlock().getRelative(event.getBlockFace().getOppositeFace());
+		Player player = event.getPlayer();
+
+		openInventory(block, player);
+
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	private void onInventoryClose(InventoryCloseEvent event) {
+		if (event.getInventory().getType().equals(InventoryType.ENDER_CHEST)) {
+			Player player = (Player) event.getPlayer();
+			Block block = enderChest.get(player.getUniqueId()).getBlock();
+			if (block != null && block.getType().equals(Material.ENDER_CHEST)) {
+				enderChest.remove(player.getUniqueId());
+				Lidded enderchest = (Lidded) block.getState();
+				enderchest.close();
+			}
+		}
+	}
+
+	private void openInventory(Block block, Player player) {
+
+		if (block.getRelative(BlockFace.UP).getType().isOccluding()) return;
 
 		if (block.getType().equals(Material.CHEST) || block.getType().equals(Material.TRAPPED_CHEST)) {
 			Chest chest = (Chest) block.getState();
 			player.openInventory(chest.getInventory());
 		} else if (block.getType().equals(Material.ENDER_CHEST)) {
+			Lidded enderchest = (Lidded) block.getState();
+			enderchest.open();
+			enderChest.put(player.getUniqueId(), block.getLocation());
 			player.openInventory(player.getEnderChest());
-			player.playSound(player.getLocation(), Sound.BLOCK_ENDER_CHEST_OPEN, 0.7f, 0.7f);
 		} else if (block.getType().equals(Material.BARREL)) {
 			Barrel barrel = (Barrel) block.getState();
 			player.openInventory(barrel.getInventory());
@@ -62,7 +106,7 @@ public class BetterItemFramesPlugin extends JavaPlugin implements Listener {
 	}
 
 	@EventHandler(priority = EventPriority.LOWEST)
-	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+	private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
 
 		if (event.isCancelled() || !(event.getDamager() instanceof Player)) return;
 
